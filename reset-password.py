3a8a8b58ab9bb7a0ebe4751a369a4b3020e9e396
Yashwanth Gandham <yashwanth.gandham1996@gmail.com>
2024-07-28 18:35:11 -0500

    added reset-password

---
+++ reset-password.py
@@ -0,0 +1,7 @@
+username = input("Enter username = ")
+passowrd = input("Enter pass = ")
+
+if username == "Admin" and password == "Admin":
+    print("You can reset")
+else:
+    print("You cannot reset")
